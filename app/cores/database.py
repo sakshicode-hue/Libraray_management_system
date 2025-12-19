88785a4acc8dd7ec8a1c2a29eab278450f887be8
Shivkumarrathod <shivarathod244@gmail.com>
2025-12-19 17:46:41 +0530

    database name aslo changed

--- app/cores/database.py
+++ app/cores/database.py
@@ -4,4 +4,4 @@ from app.cores.config import settings
 client = AsyncIOMotorClient(settings.MONGO_URI)
 db = client["Library_Management_System"]
 
-book_collection = db["books"]
+book_collection = db["Library_Management_System"]
